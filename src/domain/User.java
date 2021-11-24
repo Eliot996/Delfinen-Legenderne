e7dee1f641c68857b6e0b2a15e0e4c7a326ff63a
Eliot996 <math10t6@stud.kea.dk>
2021-11-24 11:57:07 +0100

    init

--- src/domain/User.java
+++ src/domain/User.java
@@ -1,4 +1,13 @@
 package domain;
 
 public class User {
+    private String username;
+    private String password;
+    private Roles role;
+
+    public User(String username, String password, Roles role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
 }
