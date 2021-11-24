b281e1ff73ce73a56a12b7327951e068d85890cb
Eliot996 <math10t6@stud.kea.dk>
2021-11-24 11:43:52 +0100

    added disciplines to Competitor

--- src/domain/member/Competitor.java
+++ src/domain/member/Competitor.java
@@ -1,13 +1,20 @@
 package domain.member;
 
+import domain.Discipline;
+
 import java.time.LocalDate;
+import java.util.List;
 
 public class Competitor extends Member{
+
+    private List<Discipline> disciplines;
+
     public Competitor(int memberNumber,
                       boolean isPassiveMember,
                       String name,
                       LocalDate dateOfBirth,
-                      String phoneNumber) {
+                      String phoneNumber, List<Discipline> disciplines) {
         super(memberNumber, isPassiveMember, name, dateOfBirth, phoneNumber);
+        this.disciplines = disciplines;
     }
 }
