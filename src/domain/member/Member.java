7ea97e778526179fe607b98ed2d243cc3d5ab16d
Eliot996 <math10t6@stud.kea.dk>
2021-11-24 11:40:45 +0100

    added attributes and contructor

--- src/domain/member/Member.java
+++ src/domain/member/Member.java
@@ -1,5 +1,23 @@
 package domain.member;
 
-public class Member {
+import java.time.LocalDate;
 
+public class Member {
+private int memberNumber;
+private boolean isPassiveMember;
+private String name;
+private LocalDate dateOfBirth;
+private String phoneNumber;
+
+    public Member(int memberNumber,
+                  boolean isPassiveMember,
+                  String name,
+                  LocalDate dateOfBirth,
+                  String phoneNumber) {
+        this.memberNumber = memberNumber;
+        this.isPassiveMember = isPassiveMember;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+    }
 }
