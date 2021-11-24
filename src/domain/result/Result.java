4f55e045dfaed40a57fa2c2afef8f97a51382fd1
Eliot996 <math10t6@stud.kea.dk>
2021-11-24 11:51:27 +0100

    added attributes and contructors

--- src/domain/result/Result.java
+++ src/domain/result/Result.java
@@ -1,4 +1,28 @@
 package domain.result;
 
+import domain.Discipline;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
 public class Result {
+    private UUID id;
+    private LocalTime time;
+    private LocalDate date;
+    private Competition competition;
+    private Discipline discipline;
+
+    public Result(LocalTime time, LocalDate date, Discipline discipline) {
+        this.time = time;
+        this.date = date;
+        this.discipline = discipline;
+    }
+
+    public Result(UUID id, LocalTime time, LocalDate date, Discipline discipline) {
+        this.id = id;
+        this.time = time;
+        this.date = date;
+        this.discipline = discipline;
+    }
 }
