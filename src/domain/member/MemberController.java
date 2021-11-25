053a786dff9943c2ccca38f6d6723cb67195c146
Eliot996 <math10t6@stud.kea.dk>
2021-11-25 14:47:45 +0100

    added a addMember

--- src/domain/member/MemberController.java
+++ src/domain/member/MemberController.java
@@ -1,5 +1,23 @@
 package domain.member;
 
+import database.MemberDatabase;
+
+import java.time.LocalDate;
+
 public class MemberController{
+
+    private MemberDatabase memberDB = new MemberDatabase();
+
+    // TODO: 25/11/2021 add date parsing
+    public void addMember(int memberNumber,
+                          boolean isPassiveMember,
+                          String name,
+                          LocalDate dateOfBirth,
+                          String phoneNumber,
+                          String email) {
+        memberDB.addMember(new Member(memberNumber, isPassiveMember, name, dateOfBirth, phoneNumber, email));
+    }
+
+
 
 }
