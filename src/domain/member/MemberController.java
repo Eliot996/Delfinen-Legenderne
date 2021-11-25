c013d0f6c950c54963bf90bd1a24f11ea08cbc88
Eliot996 <math10t6@stud.kea.dk>
2021-11-25 13:05:23 +0100

    added test/JUNIT

--- src/domain/member/MemberController.java
+++ src/domain/member/MemberController.java
@@ -1,4 +1,6 @@
 package domain.member;
 
 public class MemberController {
+
+
 }
---
+++ test/domain/member/MemberTest.java
@@ -0,0 +1,7 @@
+package domain.member;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberTest {
+
+}
