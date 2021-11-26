a99996aacf4bc3cfc08183d13ea419152caa4bb2
Eliot996 <math10t6@stud.kea.dk>
2021-11-26 09:40:36 +0100

    added tests to parse date from a string

--- test/domain/member/MemberControllerTest.java
+++ test/domain/member/MemberControllerTest.java
@@ -3,6 +3,7 @@ package domain.member;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -41,6 +42,36 @@ class MemberControllerTest {
 
         assertEquals(memberController.getMembers().get(0).getMemberNumber(), 0);
         assertEquals(memberController.getMembers().get(1).getMemberNumber(), 1);
+    }
+
+    @Test
+    void dateParserTest_legal() {
+        MemberController memberController = new MemberController();
+
+        String dateToParse = "06-10-1996";
+        LocalDate expected = LocalDate.of(1996, 10, 6);
+
+        LocalDate result = memberController.getDateFromString(dateToParse);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void dateParserTest_illegal() {
+        MemberController memberController = new MemberController();
+
+        String dateToParse = "06/10/1996";
+
+        assertThrows(DateTimeParseException.class, () -> memberController.getDateFromString(dateToParse));
+    }
+
+    @Test
+    void dateParserTest_reverseOrder() {
+        MemberController memberController = new MemberController();
+
+        String dateToParse = "1996-10-06";
+
+        assertThrows(DateTimeParseException.class, () -> memberController.getDateFromString(dateToParse));
     }
 
 
