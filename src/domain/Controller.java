c6b714d0b09954507c91b54fb92226b259eb6f45
Eliot996 <math10t6@stud.kea.dk>
2021-11-30 12:12:21 +0100

    Fixed Controller

--- src/domain/Controller.java
+++ src/domain/Controller.java
@@ -2,9 +2,6 @@ package domain;
 
 import domain.member.MemberController;
 import ui.UserInterface;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 //@author Sofia og Mathias
@@ -88,7 +85,6 @@ public class Controller {
     public void addMember() {
         System.out.print("is passive: ");
         boolean isPassive = ui.getBoolean();
-        userInput.nextLine(); // scanner bug
         System.out.print("name: ");
         String name = ui.getString();
         System.out.print("DOB: ");
@@ -104,41 +100,5 @@ public class Controller {
                 DOB,
                 phone,
                 email);
-    }
-
-    public void runMenu() {
-        boolean keepRunning = true;
-        while (keepRunning) {
-            int choice = ui.menu();
-            switch (choice) {
-                case 1 -> addMember();
-                case 2 -> memberController.deleteMember();
-                //case 3 -> // rediger medlem
-                case 4 -> memberController.getMembers();
-                //case 5 -> // se kontingenter
-                //case 6 ->  // se svømmeresultater
-
-                case 0 -> System.exit(0);
-            }
-        }
-    }
-
-    private ArrayList<Member> members = new ArrayList<>();
-
-    public void addMember(Member member) {
-        members.add(member);
-    }
-
-    public void deleteMember(Member member) {
-        members.remove(member);
-    }
-
-    public Member getMember(String name) {
-        for (Member member : members) {
-            if (member.getName().equals(name)) {
-                return member;
-            }
-        }
-        return null;
     }
 }
