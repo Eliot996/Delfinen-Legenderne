fc145580a665fa6722354d4afa735ee9660e7bd4
OliverBark <oliver.bark@icloud.com>
2021-11-30 09:42:28 +0100

    Igang med ResultController

--- src/domain/result/ResultController.java
+++ src/domain/result/ResultController.java
@@ -1,4 +1,38 @@
 package domain.result;
 
+import domain.Discipline;
+import domain.member.Member;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ResultController {
+
+    public void addCompetition (String competitionName,
+                                String competitionAdress,
+                                LocalDateTime dateOfCompetetition,
+                                Discipline competitionDiscipline) {
+
+        addCompetition(new Competition(competitionName, competitionAdress, dateOfCompetetition,competitionDiscipline));
+    }
+
+    public List<Competition> getCompetetions() {
+        return competitions;
+    }
+
+    public void deleteCompetitions(){
+        
+    }
+
+
+    private ArrayList<Competition> competitions = new ArrayList<>();
+
+    public void addCompetition(Competition competition){
+        competitions.add(competition);
+    }
+
+    public void deleteCompetetion(Competition competition){
+        competitions.remove(competition);
+    }
 }
