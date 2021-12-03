d1600fdcd1ffe0883b57b6d5563daefb135c70f5
sofiaabrahamsen <sofia.abrahamsen@yahoo.com>
2021-12-03 10:25:47 +0100

    startet på filehandler

--- src/database/fileHandler.java
+++ src/database/fileHandler.java
@@ -1,7 +1,19 @@
 package database;
 
+import java.io.File;
+import java.io.IOException;
+
 public class fileHandler {
+    private final String USERS = "data/users.csv";
+    private final String MEMBERS = "data/members.csv";
+    private final String TRAINERS = "data/trainers.csv";
+    private final String COMPETITORS = "data/competitors.csv";
+    private final String TEAMS = "data/teams.csv";
+    private final String COMPETITIONS = "data/competitions.csv";
+    private final String COMPETITORS_RESULTS = "data/competitor_results.csv";
+    private final String CONTINGENT = "data/contingent.csv";
 }
+// Delfinen
 /*
 Dolphine:
 https://www.asciiart.eu/animals/dolphins
