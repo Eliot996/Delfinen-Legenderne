d39743469cb15871a393eddae7657cb3a03e92ac
Eliot996 <math10t6@stud.kea.dk>
2021-12-06 10:51:12 +0100

    added constructors

--- src/domain/result/Result.java
+++ src/domain/result/Result.java
@@ -1,6 +1,7 @@
 package domain.result;
 
 import domain.Discipline;
+import domain.member.Competitor;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -8,21 +9,32 @@ import java.util.UUID;
 
 public class Result {
     private UUID id;
+    private Competitor competitor;
     private LocalTime time;
     private LocalDate date;
     private Competition competition;
     private Discipline discipline;
 
-    public Result(LocalTime time, LocalDate date, Discipline discipline) {
+    public Result(Competitor competitor, LocalTime time, LocalDate date, Competition competition, Discipline discipline) {
+        this.id = UUID.randomUUID();
+        this.competitor = competitor;
         this.time = time;
         this.date = date;
+        this.competition = competition;
         this.discipline = discipline;
     }
 
-    public Result(UUID id, LocalTime time, LocalDate date, Discipline discipline) {
-        this.id = id;
-        this.time = time;
-        this.date = date;
-        this.discipline = discipline;
+    public Result(String csv) {
+        String[] elemets = csv.split(";");
+
+        this.id = UUID.fromString(elemets[0]);
+        this.time = LocalTime.parse(elemets[1]);
+        this.date = LocalDate.parse(elemets[2]);
+        //this.competition = Competition; todo connect to competion
+        this.discipline = Discipline.valueOf(elemets[4]);
+    }
+
+    public String toCSV() {
+        return id.toString() + ";" + competitor + ";" + time.toString() + ";" + date.toString() + ";" + competition.getId() + ";" + discipline;
     }
 }
