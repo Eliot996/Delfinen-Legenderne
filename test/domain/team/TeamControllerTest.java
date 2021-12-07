0d16e8fc4b2b378c78d74e416a8e4b8700c9a4ac
Eliot996 <math10t6@stud.kea.dk>
2021-12-07 10:57:03 +0100

    added testInitTeams_SingleMemberAndTrainer

--- test/domain/team/TeamControllerTest.java
+++ test/domain/team/TeamControllerTest.java
@@ -1,6 +1,13 @@
 package domain.team;
+
+import domain.member.Member;
 import domain.member.MemberController;
+import domain.member.Trainer;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 // @Author Sofia
@@ -14,25 +21,53 @@ class TeamControllerTest {
     // ***************
 
     // test for creating 1 team
-@Test
-    void createTeamTest_With1Team(){
-    TeamController teamController = new TeamController(new MemberController());
-    teamController.addTeam("name",
-                           "description");
-    assertNotNull(teamController.getTeams().get(0));
-}
+    @Test
+    void createTeamTest_With1Team() {
+        TeamController teamController = new TeamController(new MemberController());
+        teamController.addTeam("name",
+                "description");
+        assertNotNull(teamController.getTeams().get(0));
+    }
 
     // test for creating 2 teams
-@Test
-    void createTeamTest_With2Teams(){
-    TeamController teamController = new TeamController(new MemberController());
-    teamController.addTeam("name1",
-                           "description1");
+    @Test
+    void createTeamTest_With2Teams() {
+        TeamController teamController = new TeamController(new MemberController());
+        teamController.addTeam("name1",
+                "description1");
 
-    teamController.addTeam("name2",
-                           "description2");
+        teamController.addTeam("name2",
+                "description2");
 
-    assertNotNull(teamController.getTeams().get(0));
-    assertNotNull(teamController.getTeams().get(1));
-}
+        assertNotNull(teamController.getTeams().get(0));
+        assertNotNull(teamController.getTeams().get(1));
+    }
+
+    @Test
+    void testInitTeams_SingleMemberAndTrainer() {
+        MemberController memberController = new MemberController();
+        TeamController teamController = new TeamController(memberController);
+
+        Member member1 = new Member(true,
+                "Name1",
+                LocalDate.of(2222, 11, 1),
+                "+12340878734",
+                "mail");
+        memberController.addMember(member1);
+
+        Trainer trainer1 = new Trainer(true,
+                "TrainerName2",
+                LocalDate.of(2222, 11, 1),
+                "+12340878734",
+                "mail");
+        memberController.addTrainer(trainer1);
+
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("name;description;" + trainer1.getMemberID() + ";" + member1.getMemberID());
+
+        teamController.initTeams(strings);
+
+        assertEquals(teamController.getTeamFromIndex(0).getTrainers().get(0), trainer1);
+        assertEquals(teamController.getTeamFromIndex(0).getMembers().get(0), member1);
+    }
 }
