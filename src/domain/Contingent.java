e1a48838123ee1e15e00b4087289d545718f3a7a
Eliot996 <math10t6@stud.kea.dk>
2021-12-09 09:03:14 +0100

    Created charge class

--- src/domain/Contingent.java
+++ src/domain/Contingent.java
@@ -1,5 +1,9 @@
 package domain;
 
+import domain.member.Member;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,6 +11,68 @@ import java.util.Map;
 
 public class Contingent {
     HashMap<String, Integer> contingent = new HashMap<>();
+    ArrayList<Charge> charges = new ArrayList<>();
+
+    // ****************
+    // *
+    // * Charge
+    // *
+    // ****************
+
+    private class Charge {
+        private int charge;
+        private Member member;
+        private LocalDate dueDate;
+        private boolean paid = false;
+
+        public Charge(int charge, Member member, LocalDate dueDate) {
+            this.charge = charge;
+            this.member = member;
+            this.dueDate = dueDate;
+        }
+
+        public String toCSV() {
+            return charge + ";" + member.getMemberID() + ";" + dueDate.toString() + ";" + paid;
+        }
+
+        public int getCharge() {
+            return charge;
+        }
+
+        public void setCharge(int charge) {
+            this.charge = charge;
+        }
+
+        public Member getMember() {
+            return member;
+        }
+
+        public void setMember(Member member) {
+            this.member = member;
+        }
+
+        public LocalDate getDueDate() {
+            return dueDate;
+        }
+
+        public void setDueDate(LocalDate dueDate) {
+            this.dueDate = dueDate;
+        }
+
+        public boolean isPaid() {
+            return paid;
+        }
+
+        public void setPaid(boolean paid) {
+            this.paid = paid;
+        }
+    }
+
+    // ****************
+    // *
+    // * CSV
+    // *
+    // ****************
 
     public String ContigentToCSV() {
         StringBuilder sb = new StringBuilder("Navn(MÅ IKKE ÆNDRES);Pris\n");
